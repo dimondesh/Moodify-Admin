13137805c1affa145e6bdf2f40de4b3c21ed9c5c
dimondesh <dimondesh1337@gmail.com>
2026-03-07 07:29:36 +0200

    fix title

--- frontend/src/pages/AdminPage/AdminPage.tsx
+++ frontend/src/pages/AdminPage/AdminPage.tsx
@@ -20,6 +20,7 @@ import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import StatusTabContent from "./StatusTabContent";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { Helmet } from "react-helmet-async";
 
 const AdminPage = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -48,58 +49,65 @@ const AdminPage = () => {
     );
 
   return (
-    <div className="h-screen bg-[#0f0f0f] text-white flex flex-col overflow-hidden">
-      <Header />
-      <div className="flex-1 overflow-y-auto hide-scrollbar">
-        <div className="p-4 sm:p-6">
-          <DashboardStats />
-          <Tabs defaultValue="status" className="space-y-6 mt-6">
-            <TabsList className="p-1 bg-[#2a2a2a] rounded-lg">
-              <TabsTrigger
-                value="status"
-                className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
-              >
-                <Activity className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`} />
-                {isMobile ? "" : t("admin.tabs.status")}
-              </TabsTrigger>
-              <TabsTrigger
-                value="songs"
-                className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
-              >
-                <Music className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`} />
-                {isMobile ? "" : t("admin.tabs.songs")}
-              </TabsTrigger>
-              <TabsTrigger
-                value="albums"
-                className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
-              >
-                <Album className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`} />
-                {isMobile ? "" : t("admin.tabs.albums")}
-              </TabsTrigger>
-              <TabsTrigger
-                value="artists"
-                className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
-              >
-                <Users2 className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`} />
-                {isMobile ? "" : t("admin.tabs.artists")}
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="status">
-              <StatusTabContent />
-            </TabsContent>
-            <TabsContent value="songs">
-              <SongsTabContent />
-            </TabsContent>
-            <TabsContent value="albums">
-              <AlbumsTabContent />
-            </TabsContent>
-            <TabsContent value="artists">
-              <ArtistsTabContent />
-            </TabsContent>
-          </Tabs>
+    <>
+      <Helmet>
+        <title>Moodify Admin</title>
+      </Helmet>
+      <div className="h-screen bg-[#0f0f0f] text-white flex flex-col overflow-hidden">
+        <Header />
+        <div className="flex-1 overflow-y-auto hide-scrollbar">
+          <div className="p-4 sm:p-6">
+            <DashboardStats />
+            <Tabs defaultValue="status" className="space-y-6 mt-6">
+              <TabsList className="p-1 bg-[#2a2a2a] rounded-lg">
+                <TabsTrigger
+                  value="status"
+                  className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
+                >
+                  <Activity
+                    className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`}
+                  />
+                  {isMobile ? "" : t("admin.tabs.status")}
+                </TabsTrigger>
+                <TabsTrigger
+                  value="songs"
+                  className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
+                >
+                  <Music className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`} />
+                  {isMobile ? "" : t("admin.tabs.songs")}
+                </TabsTrigger>
+                <TabsTrigger
+                  value="albums"
+                  className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
+                >
+                  <Album className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`} />
+                  {isMobile ? "" : t("admin.tabs.albums")}
+                </TabsTrigger>
+                <TabsTrigger
+                  value="artists"
+                  className="data-[state=active]:bg-[#8b5cf6] data-[state=active]:text-white text-gray-300 hover:text-white"
+                >
+                  <Users2 className={`mr-2 size-4 ${isMobile ? "ml-2" : ""}`} />
+                  {isMobile ? "" : t("admin.tabs.artists")}
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="status">
+                <StatusTabContent />
+              </TabsContent>
+              <TabsContent value="songs">
+                <SongsTabContent />
+              </TabsContent>
+              <TabsContent value="albums">
+                <AlbumsTabContent />
+              </TabsContent>
+              <TabsContent value="artists">
+                <ArtistsTabContent />
+              </TabsContent>
+            </Tabs>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
